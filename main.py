297d45f0eee7004d80109522c5244a91ba2fb6ad
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-08-19 21:00:31 -0400

    get all repos

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import requests, getpass
+import requests, getpass, json
+
+API_URL = "https://api.github.com"
 
 name = raw_input("Enter your Github username: ")
 password = getpass.getpass("Enter your Github password: ")
-r = requests.get('https://api.github.com', auth=(name, password))
+r = requests.get(API_URL, auth=(name, password))
 while r.status_code != 200:
     print("Bad Credentials!")
     name = raw_input("Enter your Github username: ")
     password = getpass.getpass("Enter your Github password: ")
     r = requests.get('https://api.github.com', auth=(name, password))
 
+repos = requests.get(API_URL + '/users/' + name + '/repos', auth=(name, password))
+repoJson = json.loads(repos.text)
+repoList = []
+for x in repoJson:
+    repoList.append(x['name'])
+print repoList
