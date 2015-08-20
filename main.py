c02847a8387856b10eb5c2a28a3720a63df8862a
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-08-19 23:10:47 -0400

    Out to report

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests, getpass, json
 
 API_URL = "https://api.github.com"
 
+
+
 name = raw_input("Enter your Github username: ")
 password = getpass.getpass("Enter your Github password: ")
 r = requests.get(API_URL, auth=(name, password))
@@ -11,7 +13,7 @@ while r.status_code != 200:
     password = getpass.getpass("Enter your Github password: ")
     r = requests.get('https://api.github.com', auth=(name, password))
 
-repos = requests.get(API_URL + '/users/' + name + '/repos', auth=(name, password))
+repos = requests.get(API_URL + '/user/repos', auth=(name, password))
 repoJson = json.loads(repos.text)
 repoList = []
 for x in repoJson:
@@ -19,10 +21,20 @@ for x in repoJson:
 
 language = {}
 total = 0
+# count = 1
 for x in repoList:
     repoStat = requests.get(API_URL + '/repos/' + name + '/' + x + '/languages', auth=(name, password))
     repoStatJson = json.loads(repoStat.text)
     for y in repoStatJson:
+        # if count == 1:
+        #     print '/'
+        # elif count == 2:
+        #     print '-'
+        # elif count == 3:
+        #     print '\\'
+        # elif count == 4:
+        #     print '|'
+
         if y in language:
             language[y] += repoStatJson[y]
             total += repoStatJson[y]
@@ -33,4 +45,11 @@ for x in repoList:
 percentages = {}
 for lang in language:
     percentages[lang] = "{0:.4f}".format((language[lang] / float(total)) * 100) + " %"
-print json.dumps(percentages, indent=1)
+
+with open('report.txt', 'w') as outfile:
+    outfile.write("Overall code usage for "+name+"\n")
+    outfile.write(json.dumps(percentages, indent=1))
+    outfile.write("\nBreakdown by repo\n")
+    outfile.write(json.dumps(language, indent=1))
+
+print("Generated report.txt")
