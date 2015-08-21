c76f5d8c415e75ab8fc5110896d6e288eedea689
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-08-20 21:15:32 -0400

    parametrize

---
+++ functions.py
@@ -0,0 +1,53 @@
+import getpass, requests, json
+
+API_URL = "https://api.github.com"
+
+def authenticate():
+    name = raw_input("Enter your Github username: ")
+    password = getpass.getpass("Enter your Github password: ")
+    r = requests.get(API_URL, auth=(name, password))
+    while r.status_code != 200:
+        print("Bad Credentials!")
+        name = raw_input("Enter your Github username: ")
+        password = getpass.getpass("Enter your Github password: ")
+        r = requests.get('https://api.github.com', auth=(name, password))
+    return {'name': name, 'password': password}
+
+
+def getNamedRepos(repoList, name, password):
+    repos = requests.get(API_URL + '/user/repos?visibility=public', auth=(name, password))
+    repoJson = json.loads(repos.text)
+    for x in repoJson:
+        repoList.append(x['name'])
+
+
+def getLanguageStats(repoList, total, language, name, password):
+    for x in repoList:
+        repoStat = requests.get(API_URL + '/repos/' + name + '/' + x + '/languages', auth=(name, password))
+        repoStatJson = json.loads(repoStat.text)
+        for y in repoStatJson:
+            if y in language:
+                language[y] += repoStatJson[y]
+                total += repoStatJson[y]
+            else:
+                language[y] = repoStatJson[y]
+                total += repoStatJson[y]
+    return total
+
+def parseLanguageStats(language, percentages, total):
+    for lang in language:
+        percentages[lang] = "{0:.4f}".format((language[lang] / float(total)) * 100) + " %"
+
+
+def writeLanguageStats(name, percentages, language):
+    with open('report.txt', 'w') as outfile:
+        outfile.write('Overall code usage for '+name+':\n')
+        outfile.write(json.dumps(percentages, indent=1))
+        outfile.write('\n\nBreakdown by repo:\n')
+        outfile.write(json.dumps(language, indent=1))
+        outfile.write('\n')
+    print('Generated report.txt')
+
+
+
+
--- main.py
+++ main.py
@@ -1,55 +1,21 @@
-import requests, getpass, json
+import functions
 
-API_URL = "https://api.github.com"
-
-
-
-name = raw_input("Enter your Github username: ")
-password = getpass.getpass("Enter your Github password: ")
-r = requests.get(API_URL, auth=(name, password))
-while r.status_code != 200:
-    print("Bad Credentials!")
-    name = raw_input("Enter your Github username: ")
-    password = getpass.getpass("Enter your Github password: ")
-    r = requests.get('https://api.github.com', auth=(name, password))
-
-repos = requests.get(API_URL + '/user/repos', auth=(name, password))
-repoJson = json.loads(repos.text)
+auth = {'name':None, 'password':None}
 repoList = []
-for x in repoJson:
-    repoList.append(x['name'])
-
 language = {}
 total = 0
-# count = 1
-for x in repoList:
-    repoStat = requests.get(API_URL + '/repos/' + name + '/' + x + '/languages', auth=(name, password))
-    repoStatJson = json.loads(repoStat.text)
-    for y in repoStatJson:
-        # if count == 1:
-        #     print '/'
-        # elif count == 2:
-        #     print '-'
-        # elif count == 3:
-        #     print '\\'
-        # elif count == 4:
-        #     print '|'
-
-        if y in language:
-            language[y] += repoStatJson[y]
-            total += repoStatJson[y]
-        else:
-            language[y] = repoStatJson[y]
-            total += repoStatJson[y]
-
 percentages = {}
-for lang in language:
-    percentages[lang] = "{0:.4f}".format((language[lang] / float(total)) * 100) + " %"
 
-with open('report.txt', 'w') as outfile:
-    outfile.write("Overall code usage for "+name+"\n")
-    outfile.write(json.dumps(percentages, indent=1))
-    outfile.write("\nBreakdown by repo\n")
-    outfile.write(json.dumps(language, indent=1))
+auth = functions.authenticate()
+functions.getNamedRepos(repoList, auth['name'], auth['password'])
+total = functions.getLanguageStats(repoList, total, language, auth['name'], auth['password'])
+functions.parseLanguageStats(language, percentages, total)
+functions.writeLanguageStats(auth['name'], percentages, language)
 
-print("Generated report.txt")
+
+
+
+
+
+
+
